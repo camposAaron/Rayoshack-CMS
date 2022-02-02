07d035432c5eec9e76ce4265cd41d23f9569c191
Aaron <campos.aaron.15@gmail.com>
2022-02-01 21:40:55 -0600

    Estilos del buscador de productos

---
+++ src/app/principal/pages/productos/lista/finder/finder.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'finder',
+  templateUrl: './finder.component.html',
+  styleUrls: ['./finder.component.scss']
+})
+export class FinderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
