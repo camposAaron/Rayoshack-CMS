54c4d7a48a812603120a930b396cb9eb9b0abdae
Aaron <campos.aaron.15@gmail.com>
2022-02-11 19:26:24 -0600

    Selects no default still

--- src/app/principal/pages/productos/detalle/formulario/formulario.component.ts
+++ src/app/principal/pages/productos/detalle/formulario/formulario.component.ts
@@ -1,6 +1,10 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { HtmlTagDefinition } from '@angular/compiler';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Producto } from 'src/app/models';
+import { Observable } from 'rxjs';
+import { Categoria, Producto, Promocion } from 'src/app/models';
+import { PromocionesService } from '../../../promociones/services/promociones.service';
+import { ProductosServicesService } from '../../services/productos-services.service';
 
 
 @Component({
@@ -11,34 +15,43 @@ import { Producto } from 'src/app/models';
 export class FormularioComponent implements OnInit {
 
   @Input() producto!: Producto;
-  public formProduct!: FormGroup
+  
+  public selected: any;
+
+  formProduct: FormGroup = this.formBuilder.group({
+    marca: ['', [Validators.required]],
+    modelo: ['', [Validators.required]],
+    precio: ['', [Validators.required]],
+    descripcion: [''],
+    detalles: [''],
+    categoria: [null],
+    promocion: [null],
+    descuento: [''],
+    stock: ['', [Validators.required]]
+  });
+
+  public categorias$!: Observable<Categoria[]>;
+  public promociones$!: Observable<Promocion[]>;
+
 
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private _productServive: ProductosServicesService,
+    private _promoService: PromocionesService
   ) { }
 
   ngOnInit(): void {
-    this.formProduct = this.formBuilder.group({
-      marca: ['', [Validators.required]],
-      modelo: ['', [Validators.required]],
-      precio: ['', [Validators.required]],
-      descripcion: ['', [Validators.required]],
-      detalles: ['', [Validators.required]],
-      categoria: ['', [Validators.required]],
-      promocion: [''],
-      descuento: [''],
-      stock: ['', [Validators.required]]
-    });
-
+    //inicializando los observables
+    this.categorias$ = this._productServive.getCategories();
+    this.promociones$ = this._promoService.getPromociones();
+    //inicualizando los inputs
     this.setProductData();
+     this.selected = this.producto.categoria;
   }
-
+  
   setProductData() {
-    this.formProduct.patchValue(this.producto)      
-  }
-
-  getSelectsData(){
-        
+    this.formProduct.patchValue(this.producto);
+    this.formProduct.controls['categoria'].setValue(this.producto.categoria);
   }
 
   save() {
