6c113557e48523d3cd475dbc41fbcf5fe6c9e9aa
Aaron <campos.aaron.15@gmail.com>
2022-02-16 21:38:39 -0600

    imagenes sin base64

--- src/app/principal/pages/productos/detalle/imagenes/imagenes.component.ts
+++ src/app/principal/pages/productos/detalle/imagenes/imagenes.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Producto } from 'src/app/models';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-imagenes',
@@ -10,16 +11,66 @@ export class ImagenesComponent implements OnInit {
 
   @Input() producto!: Producto;
   public carrete!: string[];
+  public previsualImages!: string[];
 
-  constructor() { }
+
+  constructor(
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
     this.carrete = this.producto.galeria;
-    console.log(this.producto.galeria);
   }
 
-  outOfCarrete(img: string){
-    console.log(img);
+  //sacar imagen de carrete
+  outOfCarrete(img: string) {
+    this.carrete.splice(this.carrete.indexOf(img), 1);
   }
 
+  fileCapture(event: any): any {
+    const imagen = event.target.files[0];
+
+    console.log(imagen);
+
+    if (['image/jpeg','image/png','image/jpg'].includes(imagen.type)) {
+      console.log('Si es una imagen');
+      this.carrete.push(imagen)
+      this.blobFile(imagen).then((res: any) => {
+        this.carrete.push = res.base;
+      });
+    } else {
+      console.log('No es imagen');
+    }
+    console.log(event.target.files);
+  }
+
+
+  blobFile = async ($event: any) => new Promise((resolve, reject) => {
+    try {
+      const unsafeImg = window.URL.createObjectURL($event);
+      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+      const reader = new FileReader();
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          blob: $event,
+          image,
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          blob: $event,
+          image,
+          base: null
+        });
+      };
+
+    } catch (e) {
+      reject({
+        error: e
+      })
+
+    }
+  })
 }
