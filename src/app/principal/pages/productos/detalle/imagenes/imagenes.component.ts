ab4f8c5c72df8d1c7fd0c0bb411e7a93da22a843
Aaron <campos.aaron.15@gmail.com>
2022-02-16 22:23:24 -0600

    ya puedo previsualizar

--- src/app/principal/pages/productos/detalle/imagenes/imagenes.component.ts
+++ src/app/principal/pages/productos/detalle/imagenes/imagenes.component.ts
@@ -11,66 +11,47 @@ export class ImagenesComponent implements OnInit {
 
   @Input() producto!: Producto;
   public carrete!: string[];
-  public previsualImages!: string[];
+  public previsualGalery!: string[];
 
 
   constructor(
-    private sanitizer: DomSanitizer
   ) { }
 
   ngOnInit(): void {
-    this.carrete = this.producto.galeria;
+    this.carrete = [];
+    this.previsualGalery = [];
   }
 
   //sacar imagen de carrete
   outOfCarrete(img: string) {
-    this.carrete.splice(this.carrete.indexOf(img), 1);
+    this.previsualGalery.splice(this.carrete.indexOf(img), 1);
   }
 
+  //capturar imagenes y previsualizarlas
   fileCapture(event: any): any {
-    const imagen = event.target.files[0];
+    const file = event.target.files[0];
 
-    console.log(imagen);
+    console.log(file);
+    this.carrete.push(file);
+    
 
-    if (['image/jpeg','image/png','image/jpg'].includes(imagen.type)) {
+    if (['image/jpeg','image/png','image/jpg'].includes(file.type)) {
       console.log('Si es una imagen');
-      this.carrete.push(imagen)
-      this.blobFile(imagen).then((res: any) => {
-        this.carrete.push = res.base;
-      });
+      
+      const reader = new FileReader();
+      reader.onload = () =>{
+        this.previsualGalery.push(reader.result as string);
+      }
+      reader.readAsDataURL(file);
+    
     } else {
       console.log('No es imagen');
     }
     console.log(event.target.files);
   }
 
-
-  blobFile = async ($event: any) => new Promise((resolve, reject) => {
-    try {
-      const unsafeImg = window.URL.createObjectURL($event);
-      const image = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
-      const reader = new FileReader();
-      reader.readAsDataURL($event);
-      reader.onload = () => {
-        resolve({
-          blob: $event,
-          image,
-          base: reader.result
-        });
-      };
-      reader.onerror = error => {
-        resolve({
-          blob: $event,
-          image,
-          base: null
-        });
-      };
-
-    } catch (e) {
-      reject({
-        error: e
-      })
-
-    }
-  })
 }
+
+
+
+
